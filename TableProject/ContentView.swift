a12a0c8566aae59ff1a3d23bf1aa28a8f3f8e533
Karin Prater <gahntpo@yahoo.de>
2023-06-19 10:18:08 +0300

    Initial Commit

---
+++ TableProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TableProject
+//
+//  Created by Karin Prater on 19.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TableProject/TableProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableProjectApp.swift
+//  TableProject
+//
+//  Created by Karin Prater on 19.06.23.
+//
+
+import SwiftUI
+
+@main
+struct TableProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
